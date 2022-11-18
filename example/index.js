3d9175df0d9274acf6bd2521376870bf9e059029
cawfree <hello@cawfree.com>
2022-11-18 00:18:59 +0000

    chore: update example

--- example/index.js
+++ example/index.js
@@ -1,3 +1,5 @@
+import '@ethersproject/shims';
+
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
 import { StyleSheet, View } from 'react-native';
-import { getBlockNumber, start } from 'react-native-helios';
+import { start } from 'react-native-helios';
+import { ethers } from 'ethers';
 
 export default function App() {
   React.useEffect(
@@ -13,10 +14,21 @@ export default function App() {
               'https://eth-mainnet.g.alchemy.com/v2/NgIm1_QkhzUdPRm6-WFftLQ6IpB5X712',
             consensus_rpc_url: 'https://www.lightclientdata.org',
           });
-          console.warn('Started!');
 
-          const blockNumber = await getBlockNumber();
-          console.warn(`Block number is: ${blockNumber}!`);
+          const provider = await ethers.providers.getDefaultProvider(
+            'http://127.0.0.1:8545'
+          );
+
+          const [blockNumber, balance] = await Promise.all([
+            provider.getBlockNumber(),
+            provider.getBalance('cawfree.eth'),
+          ]);
+
+          console.warn(
+            `Block number is: ${blockNumber} and balance is ${ethers.utils.formatEther(
+              balance
+            )}Ξ!`
+          );
         } catch (e) {
           console.error(e);
         }

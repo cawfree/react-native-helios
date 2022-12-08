8db98a57c8717ca6735eafc4276428263d14b105
cawfree <hello@cawfree.com>
2022-12-08 07:40:22 +0000

    chore: check in shutdown

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -32,8 +32,6 @@ export default function App() {
             )
           );
 
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-
           const urls = ENVIRONMENTS.map(
             ({ rpc_port }) =>
               `http://${
